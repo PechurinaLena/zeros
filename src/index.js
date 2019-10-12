95ac1d0e66a32495c1f12f728faa99aaa7aac01d
PechurinaLena <lena8way@gmail.com>
2019-10-12 20:48:00 +0300

    feat: some passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+	// your solution
+
+	let result = 0;
+
+	if (expression % 5 === 0) {
+		while (expression) {
+			result *= expression++;
+		}
+	} else if (expression % 2 === 0) {
+		result *= expression % 2;
+	}
+
+	for (let i = 5; i < expression; i *= 5) {
+		result += Math.floor(expression / i);
+	}
+	return result;
+};
